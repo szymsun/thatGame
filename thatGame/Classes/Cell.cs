1d678e5cec3f009aae8ce67d09e628bcfda535c8
szymsun <72306436+szymsun@users.noreply.github.com>
2023-12-17 16:25:03 +0100

    ASD

---
+++ thatGame/Classes/Cell.cs
@@ -0,0 +1,12 @@
+﻿namespace thatGame.Classes;
+
+public struct Cell
+{
+    private bool isOccupied;
+    public int[,] CellPosition;
+
+    public bool GetOccupation()
+    {
+        return isOccupied;
+    }
+}
